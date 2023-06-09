0172123c00fc2077f2fe50dd3a432b452f35c57d
BarathElangovan <barath.elangovan@outlook.com>
2023-06-09 22:59:31 +0530

    added login.js in routes, changes in server.js file

---
+++ server/routes/login.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const router = express.Router();
+
+const Users = require('../models')
+
+router.post('/', async(req,res)=>{
+    const Email = req.body.email;
+    const user = await Users.findAll({where:{email:Email}});
+    if (user.password == req.body.Password){
+        res.json("login successful")
+    }else{
+        res.json("user not found: signup")
+    }
+})
+
+
+
+return Users;
--- server/server.js
+++ server/server.js
@@ -7,9 +7,11 @@ app.use(cors())
 
 const db = require('./models');
 
-const users_router = require("./routes/Users")
+const users_route = require("./routes/Users")
+const login_route = require("./routes/login")
 
 app.use("/Users",users_router)
+app.use("./Users",login_route)
 
 
 db.sequelize.sync().then(()=>{
